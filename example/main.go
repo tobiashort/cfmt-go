cbecdced0a8fe45cb99d4c3efdbbe8bcd46d536a
Tobias Hort <mail@tobiashort.com>
2025-04-07 21:08:58 +0200

    initial commit

---
+++ example/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tobiashort/cfmt"
+)
+
+func main() {
+	cfmt.Print("This ", "is ", "#r{red}\n")
+	cfmt.Print("This ", "is ", "#g{green}\n")
+	cfmt.Print("This ", "is ", "#y{yellow}\n")
+	cfmt.Print("This ", "is ", "#b{blue}\n")
+	cfmt.Print("This ", "is ", "#p{purple}\n")
+	cfmt.Print("This ", "is ", "#c{cyan}\n")
+
+	fmt.Printf("\n")
+
+	cfmt.Printf("This is #r{%s}\n", "red")
+	cfmt.Printf("This is #g{%s}\n", "green")
+	cfmt.Printf("This is #y{%s}\n", "yellow")
+	cfmt.Printf("This is #b{%s}\n", "blue")
+	cfmt.Printf("This is #p{%s}\n", "purle")
+	cfmt.Printf("This is #c{%s}\n", "cyan")
+
+	fmt.Printf("\n")
+
+	cfmt.Println("This", "is", "#r{red}")
+	cfmt.Println("This", "is", "#g{green}")
+	cfmt.Println("This", "is", "#y{yellow}")
+	cfmt.Println("This", "is", "#b{blue}")
+	cfmt.Println("This", "is", "#p{purple}")
+	cfmt.Println("This", "is", "#c{cyan}")
+
+	fmt.Printf("\n")
+
+	cfmt.Fprint(os.Stdout, "This ", "is ", "#r{red}\n")
+	cfmt.Fprint(os.Stdout, "This ", "is ", "#g{green}\n")
+	cfmt.Fprint(os.Stdout, "This ", "is ", "#y{yellow}\n")
+	cfmt.Fprint(os.Stdout, "This ", "is ", "#b{blue}\n")
+	cfmt.Fprint(os.Stdout, "This ", "is ", "#p{purple}\n")
+	cfmt.Fprint(os.Stdout, "This ", "is ", "#c{cyan}\n")
+
+	fmt.Printf("\n")
+
+	cfmt.Fprintf(os.Stdout, "This is #r{%s}\n", "red")
+	cfmt.Fprintf(os.Stdout, "This is #g{%s}\n", "green")
+	cfmt.Fprintf(os.Stdout, "This is #y{%s}\n", "yellow")
+	cfmt.Fprintf(os.Stdout, "This is #b{%s}\n", "blue")
+	cfmt.Fprintf(os.Stdout, "This is #p{%s}\n", "purle")
+	cfmt.Fprintf(os.Stdout, "This is #c{%s}\n", "cyan")
+
+	fmt.Printf("\n")
+
+	cfmt.Fprintln(os.Stdout, "This", "is", "#r{red}")
+	cfmt.Fprintln(os.Stdout, "This", "is", "#g{green}")
+	cfmt.Fprintln(os.Stdout, "This", "is", "#y{yellow}")
+	cfmt.Fprintln(os.Stdout, "This", "is", "#b{blue}")
+	cfmt.Fprintln(os.Stdout, "This", "is", "#p{purple}")
+	cfmt.Fprintln(os.Stdout, "This", "is", "#c{cyan}")
+
+	fmt.Printf("\n")
+
+	fmt.Print(cfmt.Sprint("This ", "is ", "#r{red}\n"))
+	fmt.Print(cfmt.Sprint("This ", "is ", "#g{green}\n"))
+	fmt.Print(cfmt.Sprint("This ", "is ", "#y{yellow}\n"))
+	fmt.Print(cfmt.Sprint("This ", "is ", "#b{blue}\n"))
+	fmt.Print(cfmt.Sprint("This ", "is ", "#p{purple}\n"))
+	fmt.Print(cfmt.Sprint("This ", "is ", "#c{cyan}\n"))
+
+	fmt.Printf("\n")
+
+	fmt.Print(cfmt.Sprintf("This is #r{%s}\n", "red"))
+	fmt.Print(cfmt.Sprintf("This is #g{%s}\n", "green"))
+	fmt.Print(cfmt.Sprintf("This is #y{%s}\n", "yellow"))
+	fmt.Print(cfmt.Sprintf("This is #b{%s}\n", "blue"))
+	fmt.Print(cfmt.Sprintf("This is #p{%s}\n", "purle"))
+	fmt.Print(cfmt.Sprintf("This is #c{%s}\n", "cyan"))
+
+	fmt.Printf("\n")
+
+	fmt.Print(cfmt.Sprintln("This", "is", "#r{red}"))
+	fmt.Print(cfmt.Sprintln("This", "is", "#g{green}"))
+	fmt.Print(cfmt.Sprintln("This", "is", "#y{yellow}"))
+	fmt.Print(cfmt.Sprintln("This", "is", "#b{blue}"))
+	fmt.Print(cfmt.Sprintln("This", "is", "#p{purple}"))
+	fmt.Print(cfmt.Sprintln("This", "is", "#c{cyan}"))
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tobiashort/cfmt
+
+go 1.24.1
