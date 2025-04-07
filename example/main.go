de432e3d520a113f5e8c02db7ffe84fd6c7434ef
Tobias Hort <mail@tobiashort.com>
2025-04-07 21:55:33 +0200

    add CPrint funcs

--- example/main.go
+++ example/main.go
@@ -35,6 +35,33 @@ func main() {
 
 	fmt.Printf("\n")
 
+	cfmt.CPrint("c", "This ", "is ", "#r{red}\n")
+	cfmt.CPrint("p", "This ", "is ", "#g{green}\n")
+	cfmt.CPrint("b", "This ", "is ", "#y{yellow}\n")
+	cfmt.CPrint("y", "This ", "is ", "#b{blue}\n")
+	cfmt.CPrint("g", "This ", "is ", "#p{purple}\n")
+	cfmt.CPrint("r", "This ", "is ", "#c{cyan}\n")
+
+	fmt.Printf("\n")
+
+	cfmt.CPrintf("c", "This is #r{red}\n")
+	cfmt.CPrintf("p", "This is #g{green}\n")
+	cfmt.CPrintf("b", "This is #y{yellow}\n")
+	cfmt.CPrintf("y", "This is #b{blue}\n")
+	cfmt.CPrintf("g", "This is #p{purple}\n")
+	cfmt.CPrintf("r", "This is #c{cyan}\n")
+
+	fmt.Printf("\n")
+
+	cfmt.CPrintln("c", "This is #r{red}")
+	cfmt.CPrintln("p", "This is #g{green}")
+	cfmt.CPrintln("b", "This is #y{yellow}")
+	cfmt.CPrintln("y", "This is #b{blue}")
+	cfmt.CPrintln("g", "This is #p{purple}")
+	cfmt.CPrintln("r", "This is #c{cyan}")
+
+	fmt.Printf("\n")
+
 	cfmt.Fprint(os.Stdout, "This ", "is ", "#r{red}\n")
 	cfmt.Fprint(os.Stdout, "This ", "is ", "#g{green}\n")
 	cfmt.Fprint(os.Stdout, "This ", "is ", "#y{yellow}\n")
@@ -59,32 +86,4 @@ func main() {
 	cfmt.Fprintln(os.Stdout, "This", "is", "#b{blue}")
 	cfmt.Fprintln(os.Stdout, "This", "is", "#p{purple}")
 	cfmt.Fprintln(os.Stdout, "This", "is", "#c{cyan}")
-
-	fmt.Printf("\n")
-
-	fmt.Print(cfmt.Sprint("This ", "is ", "#r{red}\n"))
-	fmt.Print(cfmt.Sprint("This ", "is ", "#g{green}\n"))
-	fmt.Print(cfmt.Sprint("This ", "is ", "#y{yellow}\n"))
-	fmt.Print(cfmt.Sprint("This ", "is ", "#b{blue}\n"))
-	fmt.Print(cfmt.Sprint("This ", "is ", "#p{purple}\n"))
-	fmt.Print(cfmt.Sprint("This ", "is ", "#c{cyan}\n"))
-
-	fmt.Printf("\n")
-
-	fmt.Print(cfmt.Sprintf("This is #r{%s}\n", "red"))
-	fmt.Print(cfmt.Sprintf("This is #g{%s}\n", "green"))
-	fmt.Print(cfmt.Sprintf("This is #y{%s}\n", "yellow"))
-	fmt.Print(cfmt.Sprintf("This is #b{%s}\n", "blue"))
-	fmt.Print(cfmt.Sprintf("This is #p{%s}\n", "purle"))
-	fmt.Print(cfmt.Sprintf("This is #c{%s}\n", "cyan"))
-
-	fmt.Printf("\n")
-
-	fmt.Print(cfmt.Sprintln("This", "is", "#r{red}"))
-	fmt.Print(cfmt.Sprintln("This", "is", "#g{green}"))
-	fmt.Print(cfmt.Sprintln("This", "is", "#y{yellow}"))
-	fmt.Print(cfmt.Sprintln("This", "is", "#b{blue}"))
-	fmt.Print(cfmt.Sprintln("This", "is", "#p{purple}"))
-	fmt.Print(cfmt.Sprintln("This", "is", "#c{cyan}"))
-
 }
